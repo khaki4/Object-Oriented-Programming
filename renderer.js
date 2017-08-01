8edb5025c90479ca17f862a3aebffa160d9bb7c0
정지원 <4dakto@gmail.com>
2017-08-01 22:57:14 +0900

    Renderer의 init, render method override 강제

--- renderer.js
+++ renderer.js
@@ -3,15 +3,16 @@ var Renderer = function() {
 };
 
 Renderer.prototype.init = function(todo) {
+  this._todo = todo;
   this._init(todo);
 };
 Renderer.prototype.render = function (tasks) {
   this._render(tasks);
 };
 Renderer.prototype._init = function(todo) {
-
+  throw '_init: 오버라이드 되야 함'
 };
 Renderer.prototype._render = function (tasks) {
-
+  throw '_render: 오버라이드 되야 함'
 };
 
